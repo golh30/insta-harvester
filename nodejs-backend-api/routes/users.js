94bb92882fec0f54ac163a862a7a341659bbfaa8
Hardik Gol <hardik@chromotif.com>
2017-09-27 09:00:42 +0530

    added api-ai fullfillment api

--- nodejs-backend-api/routes/users.js
+++ nodejs-backend-api/routes/users.js
@@ -10,5 +10,24 @@
      res.send(linkObj);
    });
  });
+ 
+ /* for api-ai*/
+ router.get('/apiaiFullfillment', function(req, res, next) {
+   console.log(">>>apiaiFullfillment");
+    let action = request.body.result.action;
+    const parameters = request.body.result.parameters;
+    var name = parameters.name;
+    
+    insta.getInstaPPLink(name, (link) => {
+    let responseJson = {};
+    if(link){
+    responseJson.speech = 'This message is from API.AI\'s Cloud Functions for Firebase editor!' + link + " &  " + name;
+    responseJson.displayText = 'This is from API.AI\'s Cloud Functions for Firebase editor!'+ link + " &  " + name;;
+    } else{
+      responseJson.speech = 'This message is from API.AI\'s Cloud Functions for Firebase editor! Not able to find on username ' + name;
+      responseJson.displayText = 'This is from API.AI\'s Cloud Functions for Firebase editor! Not able to find on username ' + name;
+    }
+    response.json(responseJson);  });
+});
 
  module.exports = router;
